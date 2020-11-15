0fd0cf3391f69fd72995cc9d284771156709d729
Lindsay Esterman <lindsayesterman@gmail.com>
2020-11-14 21:56:15 -0500

    cover photo

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@ import { Route, Link } from 'react-router-dom'
 import Profile from '../Profile/Profile'
 import FindFriends from '../FindFriends/FindFriends'
 import './App.css'
+import phone from '../phone.png'
 
 class App extends React.Component{
   constructor(props){
@@ -27,7 +28,9 @@ updatePassword(password){
     return (
       <div className="App">
       <Route exact path="/">
+      <Link to="/">
         <h1>Screentime Saver</h1>
+        </Link>
           <Link to="/register">
             <h3 className="front-sign-up">Register</h3>
           </Link>
@@ -35,13 +38,21 @@ updatePassword(password){
           <h3 className="front-sign-up">Login</h3>
           </Link>
           <h2 className="front-quote">Are you a digital minimalist? Compete with your friends to find out!</h2>
+          <img src="https://mockuphone.com/static/images/devices/apple-iphone7plus-silver-landscape.png"></img>
         </Route>
         <Route path="/register" component={Register}/>
         <Route path="/login"  component={Login} />
-        <Route
-            path='/profile'
-            component={props => <Profile name={this.props.name}/>}
-            />
+            <Route
+              path='/profile'
+              render={routeProps => {
+                  return(
+                    <Profile
+                      name={this.props.name}
+                      {...routeProps}
+                      /> 
+                  )
+              }}
+               />
             <Route
             path='/friends'
             component={FindFriends}
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -8,14 +8,14 @@ export default function NavBar(props){
         <Link to='/'>
             <h1 className="title">Screentime Saver</h1>
         </Link>
+        <Link to='/logout'>
+            <h4 className="logout">Log Out</h4>
+          </Link>
         <Link to='/profile'>
           <h4 className="profile">Profile</h4>
         </Link>
           <Link to='/friends'>
             <h4 className="connect">Connect with friends</h4>
-          </Link>
-          <Link to='/logout'>
-            <h4 className="logout">Log Out</h4>
           </Link>
           </div>
     )
