83f65b7630733ba7435d9ac32b002e7a6d77a64d
Lindsay Esterman <lindsayesterman@gmail.com>
2020-11-22 15:04:25 -0500

    friends page working

--- src/FriendsList/FriendsList.js
+++ src/FriendsList/FriendsList.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Friends from '../Friends/Friends'
+import UsersContext from '../usersContext'
 
 export default class FriendsList extends React.Component{
     
+    static contextType = UsersContext;
+
     render(){
         const { friends = [] } = this.props
         return (
             <ul>
-            {friends.map(friend =>
+            {this.context.friends.map(friend =>
                 <Friends
                 friend_name = {friend.friend_name}
                 key={friend.id}
